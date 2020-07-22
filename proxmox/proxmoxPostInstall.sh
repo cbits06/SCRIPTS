db984adf33d219996e34c53d06311f48deba8002
cbits06 <50491610+cbits06@users.noreply.github.com>
2020-07-22 02:04:40 +0200

    Update proxmoxPostInstall.sh

--- proxmox/proxmoxPostInstall.sh
+++ proxmox/proxmoxPostInstall.sh
@@ -27,4 +27,6 @@
 	sed -i -e 's/stretch/buster/g' /etc/apt/sources.list.d/pve-install-repo.list
 	sed -i 's/stretch/buster/g' /etc/apt/sources.list
 	apt update
+	apt -f install
 	apt dist-upgrade -y
+	apt -f install
