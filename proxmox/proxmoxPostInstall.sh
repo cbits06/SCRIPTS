7aa8d457ddbe89142863f8c89d7e5da761110d3f
cbits06 <50491610+cbits06@users.noreply.github.com>
2020-07-22 02:03:38 +0200

    2206

--- proxmox/proxmoxPostInstall.sh
+++ proxmox/proxmoxPostInstall.sh
@@ -1,29 +1,30 @@
-
-#Scripts
-wget https://raw.githubusercontent.com/cbits06/Novus/master/scripts/proxmoxNoSubscription.sh
-wget https://raw.githubusercontent.com/cbits06/Novus/master/scripts/proxmoxNoSubscriptionBuster.sh
-wget https://raw.githubusercontent.com/cbits06/Novus/master/scripts/proxmoxUpgrade.sh
-
-
 #CHANGE HOSTNAME
 	export hostName=`hostname`
 	export newHostName="Novus"
-	
+
 	cp -r /var/lib/rrdcached/db/pve2-storage/$hostName /var/lib/rrdcached/db/pve2-storage/$newHostName/
 	cp /var/lib/rrdcached/db/pve2-node/$hostName /var/lib/rrdcached/db/pve2-node/$newHostName
 	sed -i 's/'$hostName'/'$newHostName'/g' /etc/hosts /etc/hostname /etc/mailname /etc/postfix/main.cf
-	
-	
+
+
 #NoSubscription
+
+	echo "deb http://ftp.debian.org/debian stretch main contrib" > /etc/apt/sources.list
+	echo "deb http://ftp.debian.org/debian stretch-updates main contrib" >> /etc/apt/sources.list
+	echo "deb http://download.proxmox.com/debian/pve stretch pve-no-subscription" >> /etc/apt/sources.list
+	echo "deb http://security.debian.org/debian-security stretch/updates main contrib" >> /etc/apt/sources.list
 	rm /etc/apt/sources.list.d/pve-enterprise.list
-	sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+	sed -i.bak "s/if (data.status !== 'Active') {/if (false) {/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
+
 	systemctl restart pveproxy.service
-	
-#Update & upgrade
 
-apt update -y
-apt upgrade -y
+#Update & upgrade to Proxmox 6
 
-	
-
-	
+	apt update -y
+	apt upgrade -y
+	apt install -f
+	apt dist-upgrade -y
+	sed -i -e 's/stretch/buster/g' /etc/apt/sources.list.d/pve-install-repo.list
+	sed -i 's/stretch/buster/g' /etc/apt/sources.list
+	apt update
+	apt dist-upgrade -y
--- proxmox/proxmoxUpgrade.sh
+++
@@ -1,9 +0,0 @@
-#UPGRADE FROM 5.x TO 6.x
-apt update -y
-apt upgrade -y
-apt install -f
-apt dist-upgrade -y
-sed -i -e 's/stretch/buster/g' /etc/apt/sources.list.d/pve-install-repo.list 
-sed -i 's/stretch/buster/g' /etc/apt/sources.list
-apt update
-apt dist-upgrade -y
